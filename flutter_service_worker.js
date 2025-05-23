65853d314a19ffc571a3175e2114c856e1587afb
Suresh <suresh161229@gmail.com>
2025-05-23 14:08:41 +0000

    Updated web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,8 +58,8 @@ const RESOURCES = {"favicon.ico": "11105844f94ff3d799c9179ca416cf0d",
 "canvaskit/chromium/canvaskit.wasm": "f5934e694f12929ed56a671617acd254",
 "canvaskit/chromium/canvaskit.js.symbols": "4525682ef039faeb11f24f37436dca06",
 "favicon2.ico": "7bf86ae40ec541e3723c6a2baf3361e1",
-"index.html": "055ec050b62b816a2131717cf2e3532e",
-"/": "055ec050b62b816a2131717cf2e3532e",
+"index.html": "ac8867a141f5a4d2317a426e974a4726",
+"/": "ac8867a141f5a4d2317a426e974a4726",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a"};
 // The application shell files that are downloaded before a service worker can
 // start.
